5cb2a6e9bf93dae80a33af077c691c66d33bbb2c
Brenda Vang <vangsbrenda@gmail.com>
2025-05-20 11:57:36 -0500

    addding code to finance application for part 2

--- Portfolio/src/main/java/com/pluralsight/finance/Jewelry.java
+++ Portfolio/src/main/java/com/pluralsight/finance/Jewelry.java
@@ -1,5 +1,28 @@
 package com.pluralsight.finance;
 
 // extends FixedAsset super class
-public class Jewelry {
+public class Jewelry extends FixedAsset{
+    private double karat;
+
+    // constructor
+    public Jewelry(String name, double marketValue, double karat) {
+        super(name, marketValue);
+        this.karat = karat;
+    }
+
+    // getters and setters
+    public double getKarat() {
+        return karat;
+    }
+
+    public void setKarat(double karat) {
+        this.karat = karat;
+    }
+
+
+    // implementing interface methods via superclass
+    @Override
+    public double getValue() {
+        return 0;
+    }
 }
