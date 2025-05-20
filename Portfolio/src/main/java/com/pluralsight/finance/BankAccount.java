0b386b5b8fba77c170f7e86be951eb3d9a134da5
Brenda Vang <vangsbrenda@gmail.com>
2025-05-20 11:29:05 -0500

    adding derived methods

--- Portfolio/src/main/java/com/pluralsight/finance/BankAccount.java
+++ Portfolio/src/main/java/com/pluralsight/finance/BankAccount.java
@@ -1,5 +1,54 @@
 package com.pluralsight.finance;
 
 // this class will implement Valuable interface
-public class BankAccount {
+public class BankAccount implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+
+    // constructors
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit (double amount) {
+
+    }
+
+    public void withdraw (double amount) {
+        
+    }
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
 }
